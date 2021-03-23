f36e2f15145a3518bb8b08d1eeab0def84112f1f
diyorbek0309 <diyorbekolimov2000@gmail.com>
2021-03-23 12:27:55 +0500

    added linkedlist class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./Linkedlist";
 
 const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("aDaasb");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
